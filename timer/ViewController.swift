5d5a6a4807dd7601d1d5b9a4de76c27baefd7e3b
清水光二 <koji22468234@icloud.com>
2017-04-06 16:26:55 +0900

    タイマー機能完成バージョン

--- timer/ViewController.swift
+++ timer/ViewController.swift
@@ -12,15 +12,13 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var timerLabel: UILabel!
     
+    var timer: Timer!
     // タイマー用の時間のための変数
     var timer_sec: Float = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        // タイマーの作成、始動
-        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         
     }
 
@@ -32,9 +30,38 @@ class ViewController: UIViewController {
     
     // selector: #selector(updatetimer) で指定された関数
     // timeInterval: 0.1, repeats: true で指定された通り、0.1秒毎に呼び出され続ける
+    
     func updateTimer(timer: Timer) {
         self.timer_sec += 0.1
         self.timerLabel.text = String(format: "%.1f", timer_sec)
+    }
+    
+    
+    
+    @IBAction func startTimer(_ sender: Any) {
+        // 動作中のタイマーを1つに保つために、 timer が存在しない場合だけ、タイマーを生成して動作させる
+        if self.timer == nil {
+            self.timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @IBAction func pauseTimer(_ sender: Any) {
+        // タイマーを破棄
+        if self.timer != nil {
+            self.timer.invalidate()   // 現在のタイマーを破棄する
+            self.timer = nil          // startTimer() の timer == nil で判断するために、 timer = nil としておく
+        }
+    }
+    
+    @IBAction func resetTimer(_ sender: Any) {
+        // リセットボタンを押すと、タイマーの時間を0に
+        self.timer_sec = 0
+        self.timerLabel.text = String(format: "%.1f", self.timer_sec)
+        
+        if self.timer != nil {
+            self.timer.invalidate()   // 現在のタイマーを破棄する
+            self.timer = nil          // startTimer() の timer == nil で判断するために、 timer = nil としておく
+        }
     }
 
 
